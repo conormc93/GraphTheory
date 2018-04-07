a113572e430ca9ddbdfd8ceec7be0b9d308a49a0
g00291461 <g00291461@gmit.ie>
2018-04-07 22:19:58 +0100

    Array Setup

--- g00291461.go
+++ g00291461.go
@@ -1,107 +1,131 @@
 package main
-import ("fmt"
+
+import (
+	"fmt"
 )
 
-
 type state struct {
-    symbol rune
-    edge1 *state
-    edge2 *state
+	symbol rune
+	edge1  *state
+	edge2  *state
 }
 
 type nfa struct {
-    initial *state
-    accept *state
+	initial *state
+	accept  *state
 }
 
 func poregtonfa(pofix string) *nfa {
-    nfastack := []*nfa{}
+	nfastack := []*nfa{}
 
-    for _, r := range pofix {
-        switch r {
-        case '.'  :
-            frag2 := nfastack[len(nfastack)-1]
-            nfastack = nfastack[:len(nfastack)-1]
-            frag1 := nfastack[len(nfastack)-1]
-            nfastack = nfastack[:len(nfastack)-1]
+	for _, r := range pofix {
+		switch r {
+		case '.':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
 
-            frag1.accept.edge1 = frag2.initial
+			frag1.accept.edge1 = frag2.initial
 
-            nfastack = append(nfastack,&nfa{initial: frag1.initial, accept: frag2.accept})
-       
-        case '|':
-            frag2 := nfastack[len(nfastack)-1]
-            nfastack = nfastack[:len(nfastack)-1]
-            frag1 := nfastack[len(nfastack)-1]
-            nfastack = nfastack[:len(nfastack)-1]
+			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
 
-            initial := state{edge1: frag1.initial,edge2: frag2.initial}
-            accept := state{}
-            frag1.accept.edge1 = &accept
-            frag2.accept.edge1 = &accept
+		case '|':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
 
-            nfastack = append(nfastack,&nfa{initial: &initial, accept: &accept})
-       
-       case '*':
-            frag := nfastack[len(nfastack)-1]
-            nfastack = nfastack[:len(nfastack)-1]
-           
-            accept := state{}
-            initial := state{edge1: frag.initial,edge2: &accept}
-            frag.accept.edge1 = frag.initial
-            frag.accept.edge2 = &accept
+			initial := state{edge1: frag1.initial, edge2: frag2.initial}
+			accept := state{}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
 
-            nfastack = append(nfastack,&nfa{initial: &initial, accept: &accept})
-       
-       default:
-            accept := state{}
-            initial := state{symbol: r,edge1: &accept}
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
 
-            nfastack = append(nfastack,&nfa{initial: &initial, accept:&accept})
-       
-        }
+		case '*':
+			frag := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
 
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
 
-    }
-return nfastack[0]
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+
+		default:
+			accept := state{}
+			initial := state{symbol: r, edge1: &accept}
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+
+		}
+
+	}
+	return nfastack[0]
 }
 
 //Converts infix to postfix
 func intopost(infix string) string {
-    specials := map[rune]int{'*':10,'.':9,'|':8}
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
-    pofix, s := []rune{},[]rune{}
-  
+	pofix, s := []rune{}, []rune{}
 
-    for _, r := range infix {
-        switch {
-            case r == '(':
-                s = append(s, r)
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			s = append(s, r)
 
-            case r == ')':
-                for s[len(s)-1] != '(' {
-                    pofix, s = append(pofix,s[len(s)-1]),s[:len(s)-1]
-                }
-                s = s[:len(s)-1]
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
 
-            case specials[r] > 0:
-                for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
-                     pofix, s = append(pofix,s[len(s)-1]),s[:len(s)-1]
-                }
-                s = append(s,r)
-            default:
-             pofix = append(pofix, r)
-        }
-    }
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			s = append(s, r)
+		default:
+			pofix = append(pofix, r)
+		}
+	}
 
-for len(s) > 0 {
-     pofix, s = append(pofix,s[len(s)-1]),s[:len(s)-1]
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
+
+	return string(pofix)
 }
 
-  return string(pofix)
+func pomatch(po string, s string) bool {
+	ismatch := false
+	ponfa := poregtonfa(po)
+
+	current := []*state{}
+	next := []*state{}
+
+	for _, r := range s {
+		for _, c := range current {
+			if c.symbol == r {
+
+			}
+		}
+		current, next = next, []*state{}
+	}
+
+	for _, c := range current {
+		if c == ponfa.accept {
+			ismatch = true
+			break
+		}
+	}
+
+	return ismatch
 }
 
 func main() {
-    nfa := poregtonfa(intopost("a.b.c*"))
-    fmt.Println(nfa)
+	fmt.Println(pomatch("ab.c*|", "cccc"))
 }
